f8f5d64b3ae9a6d210a6d1c7b0de9a1fda27478a
James Prestwich <james@prestwi.ch>
2019-11-19 15:04:21 -0800

    refactor: break out common blocks from getters/setters

--- golang/x/relay/keeper/chain.go
+++ golang/x/relay/keeper/chain.go
@@ -15,6 +15,23 @@ func (k Keeper) emitReorg(ctx sdk.Context, prev, new, lca types.BitcoinHeader) {
 	ctx.EventManager().EmitEvent(types.NewReorgEvent(prev, new, lca))
 }
 
+func (k Keeper) getDigestByStoreKey(ctx sdk.Context, key string) (types.Hash256Digest, sdk.Error) {
+	store := k.getChainStore(ctx)
+	result := store.Get([]byte(key))
+
+	digest, err := btcspv.NewHash256Digest(result)
+	if err != nil {
+		return types.Hash256Digest{}, types.ErrBadHash256Digest(types.DefaultCodespace)
+	}
+	return digest, nil
+}
+
+func (k Keeper) setDigestByStoreKey(ctx sdk.Context, key string, digest types.Hash256Digest) {
+	// TODO: Remove this in favor of Genesis state
+	store := k.getChainStore(ctx)
+	store.Set([]byte(key), digest[:])
+}
+
 // SetGenesisState sets the genesis state
 func (k Keeper) SetGenesisState(ctx sdk.Context, genesis, epochStart btcspv.BitcoinHeader) sdk.Error {
 	store := k.getChainStore(ctx)
@@ -28,64 +45,37 @@ func (k Keeper) SetGenesisState(ctx sdk.Context, genesis, epochStart btcspv.Bitc
 
 	k.setRelayGenesis(ctx, genesis.HashLE)
 	k.setBestKnownDigest(ctx, genesis.HashLE)
+	k.setLastReorgLCA(ctx, genesis.HashLE)
 
 	return nil
-
 }
 
 // setRelayGenesis sets the first digest in the relay
 func (k Keeper) setRelayGenesis(ctx sdk.Context, relayGenesis types.Hash256Digest) {
-	// TODO: Remove this in favor of Genesis state
-	store := k.getChainStore(ctx)
-	store.Set([]byte(types.RelayGenesisStorage), relayGenesis[:])
+	k.setDigestByStoreKey(ctx, types.RelayGenesisStorage, relayGenesis)
 }
 
 // GetRelayGenesis returns the first digest in the relay
 func (k Keeper) GetRelayGenesis(ctx sdk.Context) (types.Hash256Digest, sdk.Error) {
-	store := k.getChainStore(ctx)
-	result := store.Get([]byte(types.RelayGenesisStorage))
-
-	digest, err := btcspv.NewHash256Digest(result)
-	if err != nil {
-		return types.Hash256Digest{}, types.ErrBadHash256Digest(types.DefaultCodespace)
-	}
-	return digest, nil
+	return k.getDigestByStoreKey(ctx, types.RelayGenesisStorage)
 }
 
 // setBestKnownDigest sets the best known chain tip
 func (k Keeper) setBestKnownDigest(ctx sdk.Context, bestKnown types.Hash256Digest) {
-	// TODO: Remove this in favor of Genesis state
-	store := k.getChainStore(ctx)
-	store.Set([]byte(types.BestKnownDigestStorage), bestKnown[:])
+	k.setDigestByStoreKey(ctx, types.BestKnownDigestStorage, bestKnown)
 }
 
 // GetBestKnownDigest returns the best known digest in the relay
 func (k Keeper) GetBestKnownDigest(ctx sdk.Context) (types.Hash256Digest, sdk.Error) {
-	store := k.getChainStore(ctx)
-	result := store.Get([]byte(types.BestKnownDigestStorage))
-
-	digest, err := btcspv.NewHash256Digest(result)
-	if err != nil {
-		return types.Hash256Digest{}, types.ErrBadHash256Digest(types.DefaultCodespace)
-	}
-	return digest, nil
+	return k.getDigestByStoreKey(ctx, types.BestKnownDigestStorage)
 }
 
 // setLastReorgLCA sets the latest common ancestor of the last reorg
 func (k Keeper) setLastReorgLCA(ctx sdk.Context, bestKnown types.Hash256Digest) {
-	// TODO: Remove this in favor of Genesis state
-	store := k.getChainStore(ctx)
-	store.Set([]byte(types.LastReorgLCAStorage), bestKnown[:])
+	k.setDigestByStoreKey(ctx, types.LastReorgLCAStorage, bestKnown)
 }
 
 // GetLastReorgLCA returns the best known digest in the relay
 func (k Keeper) GetLastReorgLCA(ctx sdk.Context) (types.Hash256Digest, sdk.Error) {
-	store := k.getChainStore(ctx)
-	result := store.Get([]byte(types.LastReorgLCAStorage))
-
-	digest, err := btcspv.NewHash256Digest(result)
-	if err != nil {
-		return types.Hash256Digest{}, types.ErrBadHash256Digest(types.DefaultCodespace)
-	}
-	return digest, nil
+	return k.getDigestByStoreKey(ctx, types.LastReorgLCAStorage)
 }
