b4514f9448dd23e0f1c7c21f235ddafc04b9a736
Erin Hales <erin@summa.one>
2019-12-03 14:54:14 -0700

    updated queryHeaviestFromAncestor

--- golang/x/relay/client/cli/query.go
+++ golang/x/relay/client/cli/query.go
@@ -21,9 +21,8 @@ func GetQueryCmd(queryRoute string, cdc *codec.Codec) *cobra.Command {
 		RunE:                       client.ValidateCmd,
 	}
 	relayQueryCommand.AddCommand(client.GetCommands(
-		GetCmdIsAncestor(queryRoute, cdc), // @Erin: add new query commands to this list
+		GetCmdIsAncestor(queryRoute, cdc),
 		GetCmdFindAncestor(queryRoute, cdc),
-		// GetCmd(queryRoute, cdc)
 	)...)
 	return relayQueryCommand
 }
@@ -141,6 +140,78 @@ func GetCmdFindAncestor(queryRoute string, cdc *codec.Codec) *cobra.Command {
 			}
 
 			var out types.QueryResFindAncestor
+			cdc.MustUnmarshalJSON(res, &out)
+			return cliCtx.PrintOutput(&out)
+		},
+	}
+}
+
+// GetCmdIsAncestor returns the CLI command struct for IsAncestor
+func GetCmdHeaviestFromAncestor(queryRoute string, cdc *codec.Codec) *cobra.Command {
+	return &cobra.Command{
+		// what are the arguments. <> for required, [] for optional
+		Use:     "heaviestfromancestor <ancestor> <currentBest> <newBest> [limit]",
+		Example: "heaviestFromancestor 12..ab 34..cd 56..ef 200", // how do you use it?
+		// a help message shows on `help heaviestfromancestor`
+		Long: "Determines the heavier descendant of a common ancestor",
+		// how many arguments does it take?
+		// also useful: cobra.ExactArgs(3)
+		Args: cobra.RangeArgs(3, 4),
+		// what does it do when run?
+		RunE: func(cmd *cobra.Command, args []string) error {
+			// spin up a context
+			cliCtx := context.NewCLIContext().WithCodec(cdc)
+
+			ancestor, sdkErr := types.Hash256DigestFromHex(args[0])
+			if sdkErr != nil {
+				fmt.Print(sdkErr.Error())
+				return nil
+			}
+
+			currentBest, sdkErr := types.Hash256DigestFromHex(args[1])
+			if sdkErr != nil {
+				fmt.Print(sdkErr.Error())
+				return nil
+			}
+
+			newBest, sdkErr := types.Hash256DigestFromHex(args[2])
+			if sdkErr != nil {
+				fmt.Print(sdkErr.Error())
+				return nil
+			}
+
+			var limit uint32
+			if len(args) == 4 {
+				lim, err := strconv.ParseUint(args[3], 0, 32)
+				if err != nil {
+					fmt.Print(err.Error())
+					return nil
+				}
+				limit = uint32(lim)
+			}
+
+			params := types.QueryParamsHeaviestFromAncestor{
+				Ancestor:    ancestor,
+				CurrentBest: currentBest,
+				NewBest:     newBest,
+				Limit:       limit,
+			}
+
+			queryData, err := cdc.MarshalJSON(params)
+			if err != nil {
+				fmt.Print(err.Error())
+				return nil
+			}
+
+			// run the query. the routeString is passed as strings to our querier switch/case in `keeper/querier.go`
+			res, _, err := cliCtx.QueryWithData("custom/relay/heaviestfromancestor", queryData)
+
+			if err != nil {
+				fmt.Printf("could not determine if %s... or %s... is heaviest decendant of %s... \n", args[1][:8], args[2][:8], args[0][:8])
+				return nil
+			}
+
+			var out types.QueryResHeaviestFromAncestor
 			cdc.MustUnmarshalJSON(res, &out)
 			return cliCtx.PrintOutput(&out)
 		},
