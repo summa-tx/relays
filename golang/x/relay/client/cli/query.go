ef6c641e16fa0ea059bc679077be81376a117966
Erin Hales <erin@summa.one>
2019-12-03 13:55:35 -0700

    added cli cmd function for FindAncestor

--- golang/x/relay/client/cli/query.go
+++ golang/x/relay/client/cli/query.go
@@ -22,6 +22,7 @@ func GetQueryCmd(queryRoute string, cdc *codec.Codec) *cobra.Command {
 	}
 	relayQueryCommand.AddCommand(client.GetCommands(
 		GetCmdIsAncestor(queryRoute, cdc), // @Erin: add new query commands to this list
+		GetCmdFindAncestor(queryRoute, cdc),
 		// GetCmd(queryRoute, cdc)
 	)...)
 	return relayQueryCommand
@@ -81,6 +82,61 @@ func GetCmdIsAncestor(queryRoute string, cdc *codec.Codec) *cobra.Command {
 
 			if err != nil {
 				fmt.Printf("could not check if %s... is ancestor of %s... \n", args[1][:8], args[0][:8])
+				return nil
+			}
+
+			var out types.QueryResIsAncestor
+			cdc.MustUnmarshalJSON(res, &out)
+			return cliCtx.PrintOutput(&out)
+		},
+	}
+}
+
+func GetCmdFindAncestor(queryRoute string, cdc *codec.Codec) *cobra.Command {
+	return &cobra.Command{
+		// what are the arguments. <> for required, [] for optional
+		Use:     "findancestor <digest> <offset>",
+		Example: "isancestor 12..ab 2", // how do you use it?
+		// a help message. shows on `help isancestor`
+		Long: "Finds the ancestor of a given digest",
+		// how many arguments does it take?
+		// also useful: cobra.ExactArgs(3)
+		Args: cobra.ExactArgs(2),
+		// what does it do when run?
+		RunE: func(cmd *cobra.Command, args []string) error {
+			// spin up a context
+			cliCtx := context.NewCLIContext().WithCodec(cdc)
+
+			digest, sdkErr := types.Hash256DigestFromHex(args[0])
+			if sdkErr != nil {
+				fmt.Print(sdkErr.Error())
+				return nil
+			}
+
+			var offset uint32
+			off, err := strconv.ParseUint(args[1], 0, 32)
+			if err != nil {
+				fmt.Print(err.Error())
+				return nil
+			}
+			offset = uint32(off)
+
+			params := types.QueryParamsFindAncestor{
+				DigestLE: digest,
+				Offset:   offset,
+			}
+
+			queryData, err := cdc.MarshalJSON(params)
+			if err != nil {
+				fmt.Print(err.Error())
+				return nil
+			}
+
+			// run the query. the routeString is passed as strings to our querier switch/case in `keeper/querier.go`
+			res, _, err := cliCtx.QueryWithData("custom/relay/isancestor", queryData)
+
+			if err != nil {
+				fmt.Printf("could not find ancestor of %s... \n", args[0][:8])
 				return nil
 			}
 
