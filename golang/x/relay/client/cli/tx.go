8e32fb0736bd5340bdcbc51e5fcc45e932ed89b6
Erin Hales <erin@summa.one>
2020-01-09 15:21:10 -0700

    finished adding methods to cli

--- golang/x/relay/client/cli/tx.go
+++ golang/x/relay/client/cli/tx.go
@@ -2,6 +2,7 @@ package cli
 
 import (
 	"encoding/json"
+
 	"github.com/spf13/cobra"
 
 	"github.com/cosmos/cosmos-sdk/client"
@@ -44,7 +45,6 @@ func GetCmdIngestHeaderChain(cdc *codec.Codec) *cobra.Command {
 			txBldr := auth.NewTxBuilderFromCLI().WithTxEncoder(utils.GetTxEncoder(cdc))
 
 			// note: unmarshalling json here is undesirable, but necessary
-			// 	     for MarkNewHeaviest we want to accept text instead of json
 			var headers []types.BitcoinHeader
 			jsonErr := json.Unmarshal([]byte(args[0]), &headers)
 			if jsonErr != nil {
@@ -54,6 +54,101 @@ func GetCmdIngestHeaderChain(cdc *codec.Codec) *cobra.Command {
 			msg := types.NewMsgIngestHeaderChain(
 				cliCtx.GetFromAddress(),
 				headers,
+			)
+			err := msg.ValidateBasic()
+			if err != nil {
+				return err
+			}
+
+			return utils.GenerateOrBroadcastMsgs(cliCtx, txBldr, []sdk.Msg{msg})
+
+		},
+	}
+}
+
+// GetCmdIngestDifficultyChange creates a CLI command to ingest a difficulty change
+func GetCmdIngestDifficultyChange(cdc *codec.Codec) *cobra.Command {
+	return &cobra.Command{
+		Use:   "ingestdiffchange <prev epoch start> <json list of headers>",
+		Short: "ingest a difficulty change",
+		Long:  "Ingest a difficulty change",
+		Args:  cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cliCtx := context.NewCLIContext().WithCodec(cdc)
+
+			txBldr := auth.NewTxBuilderFromCLI().WithTxEncoder(utils.GetTxEncoder(cdc))
+
+			var prevEpochStart types.Hash256Digest
+			jsonErr := json.Unmarshal([]byte(args[1]), &prevEpochStart)
+			if jsonErr != nil {
+				return jsonErr
+			}
+
+			var headers []types.BitcoinHeader
+			jsonErr = json.Unmarshal([]byte(args[1]), &headers)
+			if jsonErr != nil {
+				return jsonErr
+			}
+
+			msg := types.NewMsgIngestDifficultyChange(
+				cliCtx.GetFromAddress(),
+				prevEpochStart,
+				headers,
+			)
+			err := msg.ValidateBasic()
+			if err != nil {
+				return err
+			}
+
+			return utils.GenerateOrBroadcastMsgs(cliCtx, txBldr, []sdk.Msg{msg})
+
+		},
+	}
+}
+
+// GetCmdMarkNewHeaviest creates a CLI command to update best known digest and LCA
+func GetCmdMarkNewHeaviest(cdc *codec.Codec) *cobra.Command {
+	return &cobra.Command{
+		Use:   "marknewheaviest <ancestor> <currentBest> <newBest> [limit]",
+		Short: "Updates best known digest and LCA",
+		Long:  "Updates best known digest and LCA",
+		Args:  cobra.RangeArgs(3, 4),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cliCtx := context.NewCLIContext().WithCodec(cdc)
+
+			txBldr := auth.NewTxBuilderFromCLI().WithTxEncoder(utils.GetTxEncoder(cdc))
+
+			// TODO: Set default limit if limit is not provided?
+
+			// note: unmarshalling json here is undesirable, but necessary
+			// 	     for MarkNewHeaviest we want to accept text instead of json
+			var ancestor types.Hash256Digest
+			jsonErr := json.Unmarshal([]byte(args[0]), &ancestor)
+			if jsonErr != nil {
+				return jsonErr
+			}
+			var currentBest types.RawHeader
+			jsonErr = json.Unmarshal([]byte(args[1]), &currentBest)
+			if jsonErr != nil {
+				return jsonErr
+			}
+			var newBest types.RawHeader
+			jsonErr = json.Unmarshal([]byte(args[2]), &newBest)
+			if jsonErr != nil {
+				return jsonErr
+			}
+			var limit uint32
+			jsonErr = json.Unmarshal([]byte(args[3]), &limit)
+			if jsonErr != nil {
+				return jsonErr
+			}
+
+			msg := types.NewMsgMarkNewHeaviest(
+				cliCtx.GetFromAddress(),
+				ancestor,
+				currentBest,
+				newBest,
+				limit,
 			)
 			err := msg.ValidateBasic()
 			if err != nil {
--- golang/x/relay/client/rest/tx.go
+++ golang/x/relay/client/rest/tx.go
@@ -11,6 +11,121 @@ import (
 	"github.com/summa-tx/relays/golang/x/relay/types"
 )
 
+type IngestHeaderChainReq struct {
+	BaseReq rest.BaseReq          `json:"base_req"`
+	Headers []types.BitcoinHeader `json:"headers"`
+	Sender  string                `json:"sender"`
+}
+
+func ingestHeaderChainHandler(cliCtx context.CLIContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req IngestHeaderChainReq
+
+		if !rest.ReadRESTReq(w, r, cliCtx.Codec, &req) {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, "failed to parse request")
+			return
+		}
+
+		baseReq := req.BaseReq.Sanitize()
+		if !baseReq.ValidateBasic(w) {
+			return
+		}
+
+		addr, err := sdk.AccAddressFromBech32(req.Sender)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		msg := types.NewMsgIngestHeaderChain(addr, req.Headers)
+		err = msg.ValidateBasic()
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		utils.WriteGenerateStdTxResponse(w, cliCtx, baseReq, []sdk.Msg{msg})
+	}
+}
+
+type IngestDifficultyChangeReq struct {
+	BaseReq rest.BaseReq          `json:"base_req"`
+	Start   types.Hash256Digest   `json:"prevEpochStart"`
+	Headers []types.BitcoinHeader `json:"headers"`
+	Sender  string                `json:"sender"`
+}
+
+func ingestDifficultyChangeHandler(cliCtx context.CLIContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req IngestDifficultyChangeReq
+
+		if !rest.ReadRESTReq(w, r, cliCtx.Codec, &req) {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, "failed to parse request")
+			return
+		}
+
+		baseReq := req.BaseReq.Sanitize()
+		if !baseReq.ValidateBasic(w) {
+			return
+		}
+
+		addr, err := sdk.AccAddressFromBech32(req.Sender)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		msg := types.NewMsgIngestDifficultyChange(addr, req.Start, req.Headers)
+		err = msg.ValidateBasic()
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		utils.WriteGenerateStdTxResponse(w, cliCtx, baseReq, []sdk.Msg{msg})
+	}
+}
+
+type MarkNewHeaviestReq struct {
+	BaseReq     rest.BaseReq        `json:"base_req"`
+	Ancestor    types.Hash256Digest `json:"ancestor"`
+	CurrentBest types.RawHeader     `json:"currentBest"`
+	NewBest     types.RawHeader     `json:"newBest"`
+	Limit       uint32              `json:"limit"`
+	Sender      string              `json:"sender"`
+}
+
+func markNewHeaviestHandler(cliCtx context.CLIContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req MarkNewHeaviestReq
+
+		if !rest.ReadRESTReq(w, r, cliCtx.Codec, &req) {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, "failed to parse request")
+			return
+		}
+
+		baseReq := req.BaseReq.Sanitize()
+		if !baseReq.ValidateBasic(w) {
+			return
+		}
+
+		addr, err := sdk.AccAddressFromBech32(req.Sender)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		msg := types.NewMsgMarkNewHeaviest(addr, req.Ancestor, req.CurrentBest, req.NewBest, req.Limit)
+		err = msg.ValidateBasic()
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		utils.WriteGenerateStdTxResponse(w, cliCtx, baseReq, []sdk.Msg{msg})
+	}
+}
+
 // type setLinkReq struct {
 // 	BaseReq rest.BaseReq   `json:"base_req"`
 // 	Header  string         `json:"header"`
@@ -45,40 +160,3 @@ import (
 // 		utils.WriteGenerateStdTxResponse(w, cliCtx, baseReq, []sdk.Msg{msg})
 // 	}
 // }
-
-type ingestHeaderChainReq struct {
-	BaseReq rest.BaseReq          `json:"base_req"`
-	Headers []types.BitcoinHeader `json:"headers"`
-	Sender  string                `json:"sender"`
-}
-
-func ingestHeaderChainHandler(cliCtx context.CLIContext) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		var req ingestHeaderChainReq
-
-		if !rest.ReadRESTReq(w, r, cliCtx.Codec, &req) {
-			rest.WriteErrorResponse(w, http.StatusBadRequest, "failed to parse request")
-			return
-		}
-
-		baseReq := req.BaseReq.Sanitize()
-		if !baseReq.ValidateBasic(w) {
-			return
-		}
-
-		addr, err := sdk.AccAddressFromBech32(req.Sender)
-		if err != nil {
-			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
-			return
-		}
-
-		msg := types.NewMsgIngestHeaderChain(addr, req.Headers)
-		err = msg.ValidateBasic()
-		if err != nil {
-			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
-			return
-		}
-
-		utils.WriteGenerateStdTxResponse(w, cliCtx, baseReq, []sdk.Msg{msg})
-	}
-}
