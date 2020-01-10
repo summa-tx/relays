a0e64ca46599bf409ba658ed159396f87e99e280
Erin Hales <erin@summa.one>
2020-01-10 13:41:25 -0700

    added getNextID function

--- golang/x/relay/keeper/requests.go
+++ golang/x/relay/keeper/requests.go
@@ -41,17 +41,14 @@ func (k Keeper) setRequest(ctx sdk.Context, spends []byte, pays []byte, paysValu
 		NumConfs:    numConfs,
 	}
 
-	// When a new request comes in, get the id and use it to store request
-	id := k.getID(ctx)
+	// When a new request comes in, get the next id and use it to store request
+	id := k.getNextID(ctx)
 
 	buf, err := json.Marshal(request)
 	if err != nil {
 		return types.ErrMarshalJSON(types.DefaultCodespace)
 	}
 	store.Set(id, buf)
-
-	// Increment the ID
-	k.incrementID(ctx)
 
 	// Emit Proof Request event
 	numID := binary.BigEndian.Uint64(id)
@@ -76,24 +73,23 @@ func (k Keeper) getRequest(ctx sdk.Context, id uint64) (types.ProofRequest, sdk.
 	return request, nil
 }
 
-func (k Keeper) incrementID(ctx sdk.Context) {
+func (k Keeper) getNextID(ctx sdk.Context) []byte {
 	store := k.getRequestStore(ctx)
-	// get id
-	id := k.getID(ctx)
+	// ID key
+	idKey := []byte(types.RequestID)
+	// if the store does not have an id, initialize one
+	if !store.Has(idKey) {
+		store.Set(idKey, []byte{0})
+	}
+	// get the id
+	id := store.Get(idKey)
 	// convert id to uint64 and add 1
 	newID := binary.BigEndian.Uint64(id) + 1
 	// convert back to bytes and store
 	b := make([]byte, 8)
 	binary.BigEndian.PutUint64(b, newID)
-	store.Set([]byte(types.RequestID), b)
-}
-
-func (k Keeper) getID(ctx sdk.Context) []byte {
-	store := k.getRequestStore(ctx)
-	id := []byte(types.RequestID)
-	if !store.Has(id) {
-		store.Set(id, []byte{0})
-	}
+	store.Set(idKey, b)
+	// return new id
 	return store.Get(id)
 }
 
