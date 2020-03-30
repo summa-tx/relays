f8d7bbf457bddef6dd177dec9042e5186f9fadbb
James Prestwich <james@prestwi.ch>
2020-03-29 18:09:38 -0700

    feature: migrations with deeply-buried starting blocks

---
+++ solidity/migrations/networkInfo.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const BN = require('bn.js');
+
+const ID_SPACE_SIZE = new BN('2', 10).pow(new BN('32', 10));
+
+const truffleConf = require('../truffle-config');
+
+const bitcoinMain = {
+  genesis: '0x00000020d208b5e50a8d3bd3a87f7a238e3f196621d0f9ffb5f302000000000000000000ee3af51ad3643a8a109935b45d9ca32b1003cda41df39dd75a17e13ba13aff4211aa585d39301c174a8ead73',
+  height: 590588,
+  epochStart: '0x704de08dc5329269011b878835be108a8202a93a0a2a1c000000000000000000',
+};
+
+const bitcoinTest = {
+  genesis: '0x0000ff3ffc663e3a0b12b4cc2c05a425bdaf51922ce090acd8fa3a8a180300000000000084080b23fc40476d284da49fedaea9f7cee3aba33a8bad1347fa54740a29f02752b4c45dfcff031a279c2b3a',
+  height: 1607272,
+  epochStart: '0x84a9ec3b82556297ea36d1377901ecaef0bb5a5cf683f9f05103000000000000'
+};
+
+module.exports = {
+  ropsten: {
+    network_id: truffleConf.networks.ropsten.network_id,
+    bitcoin: bitcoinMain,
+    firstID: ID_SPACE_SIZE.muln(truffleConf.networks.ropsten.network_id)
+  },
+  ropsten_test: {
+    network_id: truffleConf.networks.ropsten_test.network_id,
+    bitcoin: bitcoinTest,
+    firstID: ID_SPACE_SIZE.muln(truffleConf.networks.ropsten_test.network_id)
+  },
+  kovan: {
+    network_id: truffleConf.networks.kovan.network_id,
+    bitcoin: bitcoinMain,
+    firstID: ID_SPACE_SIZE.muln(truffleConf.networks.kovan.network_id)
+  },
+  kovan_test: {
+    network_id: truffleConf.networks.kovan_test.network_id,
+    bitcoin: bitcoinTest,
+    firstID: ID_SPACE_SIZE.muln(truffleConf.networks.kovan_test.network_id)
+  }
+};
--- solidity/truffle-config.js
+++ solidity/truffle-config.js
@@ -1,29 +1,24 @@
 /* eslint-disable */
-
-/**
- * Use this file to configure your truffle project. It's seeded with some
- * common settings for different networks and features like migrations,
- * compilation and testing. Uncomment the ones you need or modify
- * them to suit your project as necessary.
- *
- * More information about configuration can be found at:
- *
- * truffleframework.com/docs/advanced/configuration
- *
- * To deploy via Infura you'll need a wallet provider (like truffle-hdwallet-provider)
- * to sign your transactions before they're sent to a remote public node. Infura accounts
- * are available for free at: infura.io/register.
- *
- * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. If you're publishing your code to GitHub make sure you load this
- * phrase from a file you've .gitignored so it doesn't accidentally become public.
- *
- */
-
 require('dotenv').config();
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const infuraKey = process.env.SUMMA_RELAY_INFURA_KEY;
 const mnemonic = process.env.MNEMONIC;
+
+const ropsten = {
+  provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraKey}`),
+  network_id: 3,
+  gas: 5500000,
+  confirmations: 2,
+  timeoutBlocks: 200
+}
+
+const kovan = {
+  provider: () => new HDWalletProvider(mnemonic, `https://kovan.infura.io/v3/${infuraKey}`),
+  network_id: 42,
+  gas: 5500000,
+  confirmations: 2,
+  timeoutBlocks: 200
+}
 
 module.exports = {
   api_keys: {
@@ -32,60 +27,33 @@ module.exports = {
   plugins: [
     'truffle-plugin-verify'
   ],
-  /**
-   * Networks define how you connect to your ethereum client and let you set the
-   * defaults web3 uses to send transactions. If you don't specify one truffle
-   * will spin up a development blockchain for you on port 9545 when you
-   * run `develop` or `test`. You can ask a truffle command to use a specific
-   * network from the command line, e.g
-   *
-   * $ truffle test --network <network-name>
-   */
-
   networks: {
-    // // Uncomment to use local ganache
-    //  development: {
-    //   host: "127.0.0.1",     // Localhost (default: none)
-    //   port: 8545,            // Standard Ethereum port (default: none)
-    //   network_id: "*",       // Any network (default: none)
-    // },
-
     coverage: {
       host: "localhost",
       network_id: "*",
-      port: 8555,         // <-- If you change this, also set the port option in .solcover.js.
-      gas: 0xfffffffffff, // <-- Use this high gas value
-      gasPrice: 0x01      // <-- Use this low gas price
+      port: 8555,
+      gas: 0xfffffffffff,
+      gasPrice: 0x01
     },
 
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
-    ropsten: {
-      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraKey}`),
-      network_id: 3,       // Ropsten's id
-      gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    }
+    ropsten: ropsten,
+    ropsten_test: ropsten,
+
+    kovan: kovan,
+    kovan_test: kovan,
   },
 
-  // Set default mocha options here, use special reporters etc.
-  mocha: {
-    // timeout: 100000
-  },
+  // mocha: {
+  // },
 
-  // Configure your compilers
   compilers: {
     solc: {
-      version: "0.5.10",    // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      settings: {          // See the solidity docs for advice about optimization and evmVersion
+      version: "0.5.10",
+      settings: {
         optimizer: {
           enabled: true,
           runs: 200
         }
-      //  evmVersion: "byzantium"
       }
     }
   }
