7cd0939003e30bf8518e872a5d03d116c995f3f9
James Prestwich <james@prestwi.ch>
2020-04-07 17:30:10 -0700

    bug:add missing setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='summa-relay',
+    version='0.1.0',
+    description=('Summa minimal relay'),
+    author=["James Prestwich"],
+    license="LGPLv3.0",
+    install_requires=[
+        'aiohttp',
+        'riemann-ether',
+        'riemann-keys',
+        'mypy-extensions'],
+    packages=find_packages(),
+    package_data={'relay': ['py.typed']},
+    package_dir={'relay': 'relay'},
+    python_requires='>=3.6'
+)
