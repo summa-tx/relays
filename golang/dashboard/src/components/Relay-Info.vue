793be1a4d2bad1af53f7550db0530460388e9445
Erin Hales <erin@summa.one>
2020-04-27 13:18:07 -0600

    added explanations in tooltips

--- golang/dashboard/src/components/Relay-Info.vue
+++ golang/dashboard/src/components/Relay-Info.vue
@@ -23,7 +23,7 @@
               <template v-slot:activator="{ on }">
                 <v-icon v-on="on" size="20px">help</v-icon>
               </template>
-              <span>Something here</span>
+              <span>The most recent valid block detected by the relay</span>
             </v-tooltip>
           </v-layout>
 
@@ -57,7 +57,7 @@
               <template v-slot:activator="{ on }">
                 <v-icon v-on="on" size="20px">help</v-icon>
               </template>
-              <span>Something here</span>
+              <span>The digest of the best block, updated approximately every 5 blocks</span>
             </v-tooltip>
           </v-layout>
 
@@ -78,7 +78,7 @@
               <template v-slot:activator="{ on }">
                 <v-icon v-on="on" size="20px">help</v-icon>
               </template>
-              <span>Something here</span>
+              <span>The latest ancestral block of both the current best known digest and the previous best known digest</span>
             </v-tooltip>
           </v-layout>
           <v-flex class="relay__info__info" row>
