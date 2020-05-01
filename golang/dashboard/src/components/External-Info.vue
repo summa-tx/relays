c6eba64bad4f7f262bcaa699ba64d1de622eb712
Barbara Liau <barbara@liau.io>
2020-05-01 13:08:00 -0600

    Change 'Health Check' to 'Last Connected'

--- golang/dashboard/src/components/External-Info.vue
+++ golang/dashboard/src/components/External-Info.vue
@@ -1,24 +1,22 @@
 <template>
-  <v-card class="external-info">
-    <h3>External Info</h3>
-    <v-divider class="external-info__divider" />
+  <v-layout column>
     <v-layout>
       <p class="external-info__item mr-2">Source:</p>
       <p>{{ source }}</p>
     </v-layout>
     <v-layout>
-      <p class="external-info__item mr-2">Health Check:</p>
+      <p class="external-info__item mr-2">Last Connected:</p>
       <Display-Mins :timestamp="lastComms.external" />
     </v-layout>
     <v-layout>
       <p class="external-info__item mr-2">Block Changed:</p>
-      <Display-Mins :timestamp="currentBlock.verifiedAt" />
+      <Display-Mins :timestamp="currentBlock.time" />
     </v-layout>
     <v-layout>
       <p class="external-info__item mr-2">Height:</p>
       <p>{{ currentBlock.height || 'Unknown' }}</p>
     </v-layout>
-  </v-card>
+  </v-layout>
 </template>
 
 <script>
@@ -42,11 +40,11 @@ export default {
 </script>
 
 <style scoped>
-.external-info {
+/* .external-info {
   margin-top: 40px;
   padding: 20px;
   max-width: 500px;
-}
+} */
 
 .external-info__divider {
   margin: 10px 0 20px 0;
--- golang/dashboard/src/components/Relay-Info/Banner.vue
+++ golang/dashboard/src/components/Relay-Info/Banner.vue
@@ -5,21 +5,27 @@
     color="teal"
     dark
   >
-    <v-layout column>
-      <v-layout
-        class="banner__title"
-        row
-        justify-space-between
-        align-content-center
-      >
-        <v-layout column>
-          <h2>Relay Info</h2>
-          <v-layout>
-            <p class="mr-2">Health Check:</p>
-            <Display-Mins :timestamp="lastCommsRelay" />
-          </v-layout>
+    <v-layout
+      class="banner__title"
+      row
+      justify-space-between
+      align-content-center
+    >
+      <v-layout column>
+        <h2>
+          <span>{{ chainNet | capitalize }}</span>
+          Relay - [ {{  netType }} ]
+        </h2>
+        <v-layout>
+          <p class="mr-2">Last Connected:</p>
+          <Display-Mins :timestamp="lastCommsRelay" />
         </v-layout>
-        <Net-Type/>
+      </v-layout>
+      <v-layout column>
+        <h2>
+          Bitcoin - [ main ]
+        </h2>
+        <External-Info/>
       </v-layout>
     </v-layout>
   </v-card>
@@ -27,19 +33,31 @@
 
 <script>
 import { mapState } from 'vuex'
+import config from '@/config'
 
 export default {
   name: "RelayInfoBanner",
 
   components: {
-    NetType: () => import(/* webpackChunkName: 'Net-Type' */ '../Net-Type'),
+    ExternalInfo: () => import(/* webpackChunkName: 'External-Info' */ '@/components/External-Info'),
     DisplayMins: () => import(/* webpackChunkName: 'Display-Mins' */ '../Display-Mins')
   },
+
+  data: () => ({
+    netType: config.netType,
+    chainNet: config.chainNet
+  }),
 
   computed: {
     ...mapState({
       lastCommsRelay: state => state.info.lastComms.relay,
     })
+  },
+
+  filters: {
+    capitalize (str) {
+      return str.charAt(0).toUpperCase() + str.slice(1)
+    }
   }
 }
 </script>
