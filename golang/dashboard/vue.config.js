01fe034a691e24253f85508b486a67af630dd54a
Barbara Liau <barbara@liau.io>
2020-05-07 21:31:17 -0600

    Remove old options

--- golang/dashboard/vue.config.js
+++ golang/dashboard/vue.config.js
@@ -2,12 +2,6 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
-  pluginOptions: {
-    express: {
-      shouldServeApp: true,
-      serverDir: './server'
-    }
-  },
   devServer: {
     proxy: 'http://localhost:1317'
   }
