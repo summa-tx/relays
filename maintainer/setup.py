ac1428ab175edfbf0c5912e4be47d1311f9e5b2a
James Prestwich <james@prestwi.ch>
2020-07-02 15:44:09 -0700

    chore: bump ABI for new event name

--- maintainer/setup.py
+++ maintainer/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='summa-relay',
-    version='0.1.0',
+    version='0.2.0',
     description=('Summa minimal relay'),
     author=["James Prestwich"],
     license="LGPLv3.0",
