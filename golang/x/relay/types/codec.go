522ddbd7cdbef72f559726e201d6b8edcc55857a
Emily Williams <emag3m@gmail.com>
2021-03-04 14:08:14 -0500

    progress

--- golang/x/relay/types/codec.go
+++ golang/x/relay/types/codec.go
@@ -1,21 +1,17 @@
 package types
 
 import (
-	"github.com/cosmos/cosmos-sdk/codec"
+	ctypes "github.com/cosmos/cosmos-sdk/codec/types"
 )
 
-// ModuleCdc is the codec for the module
-var ModuleCdc = codec.New()
+func RegisterInterfaces(registry ctypes.InterfaceRegistry) {
+	registry.RegisterImplementations((*sdk.Msg)(nil),
+		&MsgIngestHeaderChain{},
+		&MsgIngestDifficultyChange{},
+		&MsgMarkNewHeaviest{},
+		&MsgNewRequest{},
+		&MsgProvideProof{},
+	)
 
-func init() {
-	RegisterCodec(ModuleCdc)
-}
-
-// RegisterCodec registers concrete types on the Amino codec
-func RegisterCodec(cdc *codec.Codec) {
-	cdc.RegisterConcrete(MsgIngestHeaderChain{}, "relay/IngestHeaderChain", nil)
-	cdc.RegisterConcrete(MsgIngestDifficultyChange{}, "relay/IngestDifficultyChange", nil)
-	cdc.RegisterConcrete(MsgMarkNewHeaviest{}, "relay/MarkNewHeaviest", nil)
-	cdc.RegisterConcrete(MsgNewRequest{}, "relay/NewRequest", nil)
-	cdc.RegisterConcrete(MsgProvideProof{}, "relay/ProvideProof", nil)
+	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
 }
